b8d6e634f3dc1f0d912e028c152accb4bb8a0d24
Samuel Gomes <samueldiego115@gmail.com>
2026-08-30 23:36:35 -0300

    adiciona rotas a mais e substitui primeira versao

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('API de tarefas no ar')
+  res.send('Olá, mundo!');
 });
 
-app.listen(3000)
+app.get('/contato', (req, res) => {
+  res.send('Obrigado por entrar em contato');
+});
+
+app.get('/sobre', (req, res) => {
+  res.send('Somos uma empresa');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
